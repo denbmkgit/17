7f3762b28894d5716446cb014d8ac8af34bc5b63
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-20 10:23:56 +0300

    Update task.py

--- app/routers/task.py
+++ app/routers/task.py
@@ -50,15 +50,15 @@ async def create_task(db: Annotated[Session, Depends(get_db)], create_task_: Cre
 
 
 @task_router.put("/update")
-async def update_task(db: Annotated[Session, Depends(get_db)], task_id: int, update_task: UpdateTaskc):
+async def update_task(db: Annotated[Session, Depends(get_db)], task_id: int, update_task_: UpdateTask):
     task = db.scalar(select(Task).where(Task.id == task_id))
     if task is None:
         return HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User was not found')
-    db.execute(update(Task).where(Task.id == task_id).values(firstname=update_task.firstname,
-                                                             lastname=update_task.lastname,
-                                                             age=update_task.age,
+    db.execute(update(Task).where(Task.id == task_id).values(firstname=update_task_.firstname,
+                                                             lastname=update_task_.lastname,
+                                                             age=update_task_.age,
                                                              ))
     db.commit()
     return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
